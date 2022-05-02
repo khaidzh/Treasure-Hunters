bd9b49a56cedf16997fdaf1a1767b60282f428ad
1x99 <104735356+1x99@users.noreply.github.com>
2022-05-02 13:00:00 +0300

    Update file.cpp
    Добавлена обработка объектов поля, невидимости.

--- file.cpp
+++ file.cpp
@@ -29,6 +29,31 @@ public:
                 }
 
         }
+        void draw_content(int i, int j, int t)
+        {
+            string c;
+            if(t == 0)
+                c = "         ";
+            if(t == 1)
+                c = "   ___[_]";
+            if(t == 2)
+                c = "    _ CID";
+
+
+                //so on
+
+
+            ch[(i-1)*4+5][(j-1)*2+1] = s[0];
+            ch[(i-1)*4+6][(j-1)*2+1] = s[1];
+            ch[(i-1)*4+7][(j-1)*2+1] = s[2];
+            ch[(i-1)*4+5][(j-1)*2+2] = s[3];
+            ch[(i-1)*4+6][(j-1)*2+2] = s[4];
+            ch[(i-1)*4+7][(j-1)*2+2] = s[5];
+            ch[(i-1)*4+5][(j-1)*2+3] = s[6];
+            ch[(i-1)*4+6][(j-1)*2+3] = s[7];
+            ch[(i-1)*4+7][(j-1)*2+3] = s[8];
+
+        }
         void draw()
         {
                 map->transpose();
@@ -68,9 +93,17 @@ public:
                                             ch[(i-1)*4+3][(j-1)*2+3] = ' ';
                                         }
 
+                                        if(map->mo[i][j]->vision() == 0)
+                                        {
+                                            ch[(i-1)*4+2][(j-1)*2+6] = '?';
+                                        }
+                                        if(map->mo[i][j]->vision() == 1)
+                                        {
+                                            draw_content(i, j, map->mo[i][j]->getCT());
+                                        }
 
                                 }
-                                if(map->mo[i][j]->whoami() == WALL)
+                                if(map->mo[i][j]->whoami() == WALL && map->mo[i][j]->vision() == 1)
                                 {
                                         if(map->mo[i][j]->getCT() == IRON_WALL)
                                         {
@@ -96,30 +129,103 @@ public:
                                             {
                                                     for(int ki = 2; ki < 8; ki++)
                                                     {
-                                                            if(ki != 6 && ki != 5)
-                                                                ch[(i-1)*4+ki][2*j] = '#';
-                                                            else
-                                                                ch[(i-1)*4+ki][2*j] = '\'';
+                                                        ch[(i-1)*4+ki][2*j] = '=';
                                                     }
                                             }
                                             else
                                             {
                                                     for(int kj = 1; kj < 4;kj++)
                                                     {
-                                                        if(kj != 3)
-                                                        {
-                                                            ch[i*4][2*(j-1)+kj] = '#';
-                                                            ch[i*4+1][2*(j-1)+kj] = '\'';
-                                                        }
+                                                        ch[i*4][2*(j-1)+kj] = '|';
+                                                        ch[i*4+1][2*(j-1)+kj] = '|';
+                                                    }
+                                            }
+                                        }
+                                        if(map->mo[i][j]->getCT() == CORRUPTED_WOOD_WALL)
+                                        {
+                                            if(i % 2 == 1)
+                                            {
+                                                    for(int ki = 2; ki < 8; ki++)
+                                                    {
+                                                        if(ki <= 3 || ki >= 7)
+                                                            ch[(i-1)*4+ki][2*j] = '=';
+                                                        if(ki == 4 || ki == 6)
+                                                            ch[(i-1)*4+ki][2*j] = '-';
+                                                        if(ki == 5)
+                                                            ch[(i-1)*4+ki][2*j] = ' ';
+                                                    }
+                                            }
+                                            else
+                                            {
+                                                    for(int kj = 1; kj < 4;kj++)
+                                                    {
+                                                        ch[i*4][2*(j-1)+kj] = ' ';
+                                                        ch[i*4+1][2*(j-1)+kj] = ' ';
+                                                    }
+                                                    ch[i*4][2*(j-1)+1] = '|';
+                                                    ch[i*4+1][2*(j-1)+3] = '|';
+
+                                            }
+                                        }
+                                        if(map->mo[i][j]->getCT() == CORRUPTED_IRON_WALL)
+                                        {
+                                            if(i % 2 == 1)
+                                            {
+                                                    for(int ki = 2; ki < 8; ki++)
+                                                    {
+                                                        if(ki <= 3 || ki >= 7)
+                                                            ch[(i-1)*4+ki][2*j] = '#';
                                                         else
-                                                        {
-                                                            ch[i*4][2*(j-1)+kj] = '#';
-                                                            ch[i*4+1][2*(j-1)+kj] = '#';
-                                                        }
+                                                            ch[(i-1)*4+ki][2*j] = '=';
+                                                    }
+                                            }
+                                            else
+                                            {
+                                                    for(int kj = 1; kj < 4;kj++)
+                                                    {
+                                                        ch[i*4][2*(j-1)+kj] = '|';
+                                                        ch[i*4+1][2*(j-1)+kj] = '|';
+                                                    }
+                                                    ch[i*4][2*(j-1)+1] = '#';
+                                                    ch[i*4+1][2*(j-1)+3] = '#';
+
+                                            }
+                                        }
+                                        if(map->mo[i][j]->getCT() == EMPTY)
+                                        {
+                                            if(i % 2 == 1)
+                                            {
+                                                    for(int ki = 2; ki < 8; ki++)
+                                                    {
+                                                            ch[(i-1)*4+ki][2*j] = ' ';
+                                                    }
+                                            }
+                                            else
+                                            {
+                                                    for(int kj = 1; kj < 4;kj++)
+                                                    {
+                                                            ch[i*4][2*(j-1)+kj] = ' ';
+                                                            ch[i*4+1][2*(j-1)+kj] = ' ';
                                                     }
                                             }
                                         }
                                 }
+                                if(map->mo[i][j]->whoami() == WALL && map->mo[i][j]->vision() == 0)
+                                {
+                                    if(i % 2 == 1)
+                                    {
+                                            for(int ki = 4; ki < 6; ki++)
+                                            {
+                                                ch[(i-1)*4+ki][2*j] = '?';
+                                            }
+                                    }
+                                    else
+                                    {
+                                        ch[i*4][2*(j-1)+2] = '?';
+                                        ch[i*4+1][2*(j-1)+2] = '?';
+                                    }
+                                }
+
                         }
                 }
                 for(int j = 0; j < 21; j++)
